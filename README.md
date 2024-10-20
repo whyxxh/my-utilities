89e88da3667e15cbf84bfa3eaa7d0ec0904fb7d4
whyxxh <rubenbeg0908@gmail.com>
2024-10-20 19:52:13 +0400

    tried to add font type prompt (italic...).

--- README.md
+++ README.md
@@ -4,11 +4,13 @@ small personal utilities to better my life
 
 **for now it includes:**
 - a markdown to pdf converter
-- and a font switcher for the kitty terminal
+- and a font switcher for the kitty terminal **not finished**
 
 the font switcher switches only the main font to the regular version of the desired font for now
 
----
-
 # todo:
-- add so you can change italic and bold fonts for the font utility
+- ~add so you can change italic and bold fonts for the font utility~ -> brought a bug
+- fix the bug where chosing regular doesn't change the regular font
+- add so it doesn't quit the program when you choose a font but so it prompts you with another type of font
+- make it so you can make templates
+- ? maybe rewrite it in python
--- font.sh
+++ font.sh
@@ -3,36 +3,50 @@
 font_dir="$HOME/.fonts/"
 config_file="$HOME/.config/kitty/kitty.conf"
 
-font_files=($(ls "$font_dir" | grep -E 'Regular'))
+font_types=("Italic" "Regular" "Bold")
 
-format_font_name() {
-  local font_file="$1"
-  local font_name="${font_file%.*}"
-  font_name="${font_name//Regular/}"
-  font_name="${font_name//Nerd/ Nerd}"
-  font_name="${font_name//Mono/ Mono}"
-  font_name="${font_name//Font/ Font}"
-  font_name="${font_name//-/ }"
-  echo "$font_name"
-}
+select font_type in "${font_types[@]}"; do
+  font_files=($(ls "$font_dir"))
 
-fonts=()
-for font_file in "${font_files[@]}"; do
+  format_font_name() {
+    local font_file="$1"
+    local font_name="${font_file%.*}"
+    if [[ "$font_type" == 'Regular' ]]; then
+      font_name="${font_name//Regular/}"
+    fi
+    font_name="${font_name//Nerd/ Nerd}"
+    font_name="${font_name//Mono/ Mono}"
+    font_name="${font_name//Font/ Font}"
+    font_name="${font_name//Propo/ Propo}"
+    font_name="${font_name//-/ }"
+    echo "$font_name"
+  }
+
+  fonts=()
+  for font_file in "${font_files[@]}"; do
     formatted_name=$(format_font_name "$font_file")
     fonts+=("$formatted_name")
-done
+  done
 
-echo "Please select a new font: "
-select font in "${fonts[@]}"; do
-  if [[ -n $font ]]; then
-    echo "You selected: $font"
-    break 
+  echo "Please select a new font: "
+  select font in "${fonts[@]}"; do
+    if [[ -n $font ]]; then
+      echo "You selected: $font"
+      break
+    else
+      echo "Invalid selection, try again: "
+    fi
+  done
+  
+  font_type=$(echo "$font_type" | tr '[:upper:]' '[:lower:]')
+
+  if [[ "$font_type" == 'Regular' ]]; then
+      sed -i.bak "s/^font_family .*/font_family $font/" "$config_file"
   else
-    echo "Invalid selection, try again: "
-  fi 
+      sed -i.bak "s/^${font_type}_font .*/${font_type}_font $font/" "$config_file"
+  fi
+
+  echo "Updated kitty.conf with font: $font"
+  break
 done
-
-sed -i.bak "s/^font_family .*/font_family $font/" "$config_file"
-
-echo "Updated kitty.conf with font: $font"
 
